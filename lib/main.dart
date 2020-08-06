cdafd1ec0b5963d2109bac50d5c7598152a15b4f
Felix Wippich <felix.wippich@me.com>
2020-08-06 17:00:07 +0200

    Insert text and pictures to Chapter2 (no styling yet) & Fix formatting issues

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,18 @@ import 'package:route_flutter_app/Chapter1.dart';
 
 void main() {
   runApp(MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Bellefair"),
-      home: HomePage(),
-      initialRoute: "/",
-      routes: {
-        '/':(context) => HomePage(),
-        '/chapter1':(context) => Chapter1()
-      }
-    ),
+        theme: ThemeData(fontFamily: "Bellefair"),
+        home: HomePage(),
+        initialRoute: "/",
+        routes: {
+          '/': (context) => HomePage(),
+          '/chapter1': (context) => Chapter1()
+        });
   }
 }
